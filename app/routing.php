7da1ad287d0ff49875746057200d17bcc6df0a05
Thiago Dieb <thiago@dieb.com.br>
2016-03-10 00:57:04 -0300

    ajuste na estrutura, consolidando as informacoes no arquivo inicial da ferramenta

--- app/routing.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Avenger\Controller;
-
-$application->add(new Avenger\Controller\PasswordCrackingController());
-$application->add(new Avenger\Controller\BruteForceController());
-$application->add(new Avenger\Controller\WordPressController());
-$application->add(new Avenger\Controller\GoogleHackingController());
-$application->add(new Avenger\Controller\LocalFileDownloadController());
-$application->add(new Avenger\Controller\SqlInjectionController());
